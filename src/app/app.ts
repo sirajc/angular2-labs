5795eff582528076688a92245869c8b6894d991a
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-08-06 03:36:06 +0530

    chore: remove all components and just keep app

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { ROUTER_DIRECTIVES } from '@angular/router';
 
 @Component({
   selector: 'app',
   pipes: [],
   providers: [],
-  directives: [ ROUTER_DIRECTIVES ],
-  templateUrl: './app.html',
+  directives: [ ],
+  templateUrl: 'app.html',
 })
 export class App {
   constructor() {}
