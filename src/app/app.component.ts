6ac390efd3450b79df01a613244784d486c53fdd
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-08-06 20:06:37 +0530

    feat: add router links and styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,6 @@ import { Component } from '@angular/core';
   providers: [],
   directives: [],
   templateUrl: 'app.component.html',
+  styleUrls: [ 'app.component.css' ]
 })
 export class AppComponent {}
