1043373c945606026acb23f229d39230b6760092
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-08-18 23:44:47 +0530

    chore: add script for building gh-pages

---
+++ build-gh-pages.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+echo -e "\e[32mPreparing the build...\e[0m"
+echo ""
+#npm run build
+echo ""
+echo -e "\e[31mCleaning gh-pages\e[0m"
+rm -rf gh-pages/*
+echo ""
+echo -e "\e[32mCopying files\e[0m"
+cp -R dist/* gh-pages/
+echo -e "\e[32mstaging files in git\e[0m"
+cd gh-pages
+git add .
+echo -e "\e[36m---------------------------------------------------------------\e[0m"
+echo -e "\e[32mGit Status ouput\e[0m"
+echo ""
+git status
+echo ""
+echo ""
+echo -e "\e[36m======================= Manual Steps ===========================\e[0m"
+echo "Add '/angular2-labs' before '/dygraph-combined.js' in index.html"
+echo -e "\e[36m================================================================\e[0m"
