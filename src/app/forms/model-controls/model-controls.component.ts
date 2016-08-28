1075294183194e14f966b54d26d4c8df679f49c9
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-08-29 04:08:47 +0530

    feat: add Model Driven Forms - Form Controls

--- src/app/forms/model-controls/model-controls.component.ts
+++ src/app/forms/model-controls/model-controls.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+
+import { validateEmailFactory } from './../../shared/validators/email.validator';
+import { CheckboxValidator } from './../../shared/validators/checkbox-checked.validator';
 
 @Component({
   selector: 'labs-model-controls',
   templateUrl: 'model-controls.component.html'
 })
-export class ModelControlsComponent {}
+export class ModelControlsComponent {
+  form: FormGroup;
+
+  constructor() {
+
+    this.form = new FormGroup({
+      email: new FormControl(null, validateEmailFactory()),
+      firstName: new FormControl(),
+      lastName: new FormControl(),
+      gender: new FormControl(),
+      notrobot: new FormControl(null, CheckboxValidator.checked),
+      address: new FormGroup({
+        addressLine1: new FormControl(),
+        addressLine2: new FormControl(),
+        city: new FormControl(),
+        zipCode: new FormControl(),
+        landmark: new FormControl(),
+        addressType: new FormControl(),
+        country: new FormControl()
+      })
+    });
+  }
+
+  signUp(data: any) {
+    console.log('Submit the data: ', data);
+  }
+}
--- src/app/forms/model-controls/model-controls.module.ts
+++ src/app/forms/model-controls/model-controls.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { ModelControlsComponent } from './model-controls.component.ts';
+import { Bootstrap4FormsModule } from '../../shared/bootstrap4/forms';
 
 import { moduleRoutes } from './model-controls.routes.ts';
 
 @NgModule({
-  imports: [ moduleRoutes ],
+  imports: [ Bootstrap4FormsModule, CommonModule, FormsModule, ReactiveFormsModule, moduleRoutes ],
   declarations: [ ModelControlsComponent ]
 })
 export class ModelControlsModule {}
