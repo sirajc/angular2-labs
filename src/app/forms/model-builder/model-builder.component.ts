6dd7cd7de0538cd44a56cb18fbc5abc48346e1bc
Sirajuddin Choudhary <sirajuddin.c@gmail.com>
2016-08-29 04:27:46 +0530

    feat: add Model Driven forms - Form Builder

--- src/app/forms/model-builder/model-builder.component.ts
+++ src/app/forms/model-builder/model-builder.component.ts
@@ -1,7 +1,37 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, FormControl } from '@angular/forms';
+
+import { validateEmailFactory } from '../../shared/validators/email.validator';
+import { CheckboxValidator } from '../../shared/validators/checkbox-checked.validator';
 
 @Component({
   selector: 'labs-model-builder',
   templateUrl: 'model-builder.component.html'
 })
-export class ModelBuilderComponent {}
+export class ModelBuilderComponent {
+  form: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+
+    this.form = this.formBuilder.group({
+      email: [null, validateEmailFactory()],
+      firstName: null,
+      lastName: null,
+      gender: null,
+      notrobot: [null, CheckboxValidator.checked],
+      address: this.formBuilder.group({
+        addressLine1: null,
+        addressLine2: null,
+        city: null,
+        zipCode: null,
+        landmark: null,
+        addressType: null,
+        country: null
+      })
+    });
+  }
+
+  signUp(data: any) {
+    console.log('Submit the data: ', data);
+  }
+}
--- src/app/forms/model-builder/model-builder.module.ts
+++ src/app/forms/model-builder/model-builder.module.ts
@@ -1,10 +1,13 @@
 import { NgModule } from '@angular/core';
-import { ModelBuilderComponent } from './model-builder.component.ts';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { ModelBuilderComponent } from './model-builder.component';
+import { Bootstrap4FormsModule } from '../../shared/bootstrap4/forms/bootstrap4-forms.module';
 
-import { moduleRoutes } from './model-builder.routes.ts';
+import { moduleRoutes } from './model-builder.routes';
 
 @NgModule({
-  imports: [ moduleRoutes ],
+  imports: [ Bootstrap4FormsModule, CommonModule, ReactiveFormsModule, moduleRoutes ],
   declarations: [ ModelBuilderComponent ]
 })
 export class ModelBuilderModule {}
